3f3d2d987c1d254a2caacce66104a560e2c45b2e
xiahua19 <jx.zhang@pku.edu.cn>
2023-08-07 16:44:44 +0800

    implement symbol_table.py

--- projects/06/src/instruction.py
+++ projects/06/src/instruction.py
@@ -1,7 +1,46 @@
-class InstructionA:
-    def __init__(self, instruction, symbol_table):
-        self.instruction = instruction
+def instruction_typeis_a(instruction : str) -> bool:
+    """Determine whether the type of an instruction is a
 
-class InstructionC:
-    def __init__(self, instruction):
+    Args:
+        instruction (str): one instruction
+
+    Returns:
+        bool: whether the type of an instruction is a
+    """
+    import re
+    a_instruction = re.compile(r'^@.*')
+    
+    if (a_instruction.match(instruction)):
+        return True
+    return False
+
+
+class InstructionA:
+    def __init__(self, instruction : str, symbol_table : list):
+        """Init this class
+
+        Args:
+            instruction (str): the a-instruction
+            symbol_table (list): symbol_table used for translate a-instruction
+        """
         self.instruction = instruction
+        self.symbol_table = symbol_table
+    
+    def translate(self) -> str:
+        symbol = self.instruction[1:]
+        value = self.symbol_table[symbol]
+        value16bit = bin(value)[2:].zfill(16)
+        return value16bit
+        
+class InstructionC:
+    def __init__(self, instruction : str):
+        """Init this class
+
+        Args:
+            instruction (str): the c-instruction
+        """
+        self.instruction = instruction
+    
+    def translate(self) -> str:
+        pass
+    
--- projects/06/src/symbol_table.py
+++ projects/06/src/symbol_table.py
@@ -1,2 +1,56 @@
-def symbol_table(full_asm):
-    pass
+from instruction import instruction_typeis_a
+from handle_whitespace import handle_whitespace
+
+def get_symbol_table(full_asm : list) -> dict:
+    """Get the symbol table of a .asm file
+
+    Args:
+        full_asm (list): list of code in a .asm file, which has been remove all whitespaces
+
+    Returns:
+        dict: symbol table {symbol : int}
+    """
+    import re
+
+    # init the symbol_table
+    symbol_table = dict()
+
+    # add all pre-defined symbols to symbol_table
+    some_defined_symbols = ['SP', 'LCL', 'ARG', 'THIS', 'THAT']
+    for i in range(len(some_defined_symbols)):
+        symbol_table[some_defined_symbols[i]] = i
+    for i in range(16):
+        symbol_table[f'R{i}'] = i
+    symbol_table['SCREEN'] = 16384
+    symbol_table['KBD'] = 24576    
+
+    label_num = 0
+    start_addr = 16
+    # select the label and variable symbols, add them to symbol_table
+    for i in range(len(full_asm)):
+        line = full_asm[i]
+
+        label_pattern = re.compile(r'^\(.*\)$')
+        if label_pattern.match(line):
+            label = line[1:][:-1]
+            address = i - label_num
+            label_num += 1
+            symbol_table[label] = address
+
+        if instruction_typeis_a(line):
+            symbol = line[1:]
+            if symbol not in symbol_table and not symbol.isdigit():
+                symbol_table[symbol] = start_addr
+                start_addr += 1
+    
+    return symbol_table
+
+if __name__ == '__main__':
+    import sys
+    args = sys.argv
+    assert len(args) == 2
+
+    handled_code = handle_whitespace(file_path=args[1])
+    print(handled_code)
+    symbol_table = get_symbol_table(full_asm=handled_code)
+    print(symbol_table)
