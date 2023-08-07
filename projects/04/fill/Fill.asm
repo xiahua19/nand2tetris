9873184fe426bc295eff6904402c9f6ac20a9aa8
xiahua19 <jx.zhang@pku.edu.cn>
2023-08-07 13:04:25 +0800

    implement project4 Fill.asm

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,73 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+(keyboard)
+@KBD
+D = M
+@keyboard
+D; JEQ
+
+(blackinit)
+@SCREEN
+D = A
+@R0
+M = D
+@8192
+D = A
+@R1
+M = D
+@SCREEN
+D = A
+@R1
+M = M + D
+
+(black)
+@R0
+A = M
+M = -1
+@R0
+D = M + 1
+M = D
+@R1
+M = M - D
+@keyboard
+M; JEQ
+@KBD
+D = M
+@whiteinit
+D; JEQ
+@black
+0; JEQ
+
+
+(whiteinit)
+@SCREEN
+D = A
+@R0
+M = D
+@8192
+D = A
+@R1
+M = D
+@SCREEN
+D = A
+@R1
+M = M + D
+
+(white)
+@R0
+A = M
+M = 0
+@R0
+D = M + 1
+M = D
+@R1
+M = M - D
+@keyboard
+M; JEQ
+@KBD
+D = M
+@blackinit
+D; JNE
+@white
+0; JEQ
